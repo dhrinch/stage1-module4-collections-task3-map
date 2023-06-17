aca7e65a696d06205d5083c72010d53962bc0842
dhrinch <dhrinch@gmail.com>
2023-06-17 03:02:18 -0400

    Finished task

--- src/main/java/com/epam/mjc/collections/map/FunctionValueFinder.java
+++ src/main/java/com/epam/mjc/collections/map/FunctionValueFinder.java
@@ -2,12 +2,20 @@ package com.epam.mjc.collections.map;
 
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class FunctionValueFinder {
     public boolean isFunctionValuePresent(List<Integer> sourceList, int requiredValue) {
         Map<Integer, Integer> functionMap = calculateFunctionMap(sourceList);
-    }
+        return functionMap.entrySet()
+                .stream()
+                .filter(s->s.getValue()==requiredValue)
+                .collect(Collectors.toSet())
+                .size()>0;}
 
     private Map<Integer, Integer> calculateFunctionMap(List<Integer> sourceList) {
+
+                return sourceList.stream()
+                .collect(Collectors.toMap(x-> x, x-> x *5+2));
     }
 }
--- src/main/java/com/epam/mjc/collections/map/WordRepetitionMapCreator.java
+++ src/main/java/com/epam/mjc/collections/map/WordRepetitionMapCreator.java
@@ -1,8 +1,21 @@
 package com.epam.mjc.collections.map;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class WordRepetitionMapCreator {
     public Map<String, Integer> createWordRepetitionMap(String sentence) {
+        if (sentence == null || sentence.isEmpty())
+            return new HashMap<>();
+        String[] words = sentence.toLowerCase().split("\\s*[\\W]+\\s*");
+        Map<String, Integer> wordRepetitionMap = new HashMap<>();
+        for (String word : words) {
+            if (wordRepetitionMap.containsKey(word)) {
+                wordRepetitionMap.put(word, wordRepetitionMap.get(word) + 1);
+            } else {
+                wordRepetitionMap.put(word, 1);
+            }
+        }
+        return wordRepetitionMap;
     }
 }
